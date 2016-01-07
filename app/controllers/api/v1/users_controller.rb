3f537b0999dad10ec9ef9b21ed6fd8be52a39194
andela-rchukwumam <aruth71@gmail.com>
2016-01-07 11:54:05 +0100

    Implementing the authentication feature

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+  respond_to :json, :xml
+
+  def create
+    @user = User.new(user_params) if user_params
+    if @user.save
+      token = Api::AuthToken.encode(user: @user.id)
+      render json: @user, meta: { token: token }, status: 201
+    else
+      response = { Status: 'User could not be created' }
+      render json: response.to_json, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:full_name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      post "auth/login"
+      get "auth/logout"
+      resources :users, only: [:create]
+        resources :lists do
+          resources :items
+        end
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
